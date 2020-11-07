cac5bb46b131ce7fe6e55aa3a81e8156b1ae4080
shahrzadabedi <shahrzad.abedi@gmail.com>
2020-11-07 16:42:32 +0330

    [revision]install and add simpleinjector

---
+++ Survey.Business/SurveyEfDbContextLocator.cs
@@ -0,0 +1,40 @@
+﻿using Survey.DataAccess;
+using Survey.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Survey.Business
+{
+    public class SurveyEfDbContextLocator : IEFRepositoryLocator<DataAccess.Survey,DataAccess.User_Survey>,IDbContextLocator
+    {
+        
+        private readonly DbContext _dbContext;
+        private IRepository<DataAccess.Survey> _surveyRep;
+        private IRepository<DataAccess.User_Survey> _user_survey;
+        public DbContext DbContext { get { return _dbContext; }  }
+
+     
+
+        //public IRepository<TSurvey> SurveyRep { get { return _surveyRep; } }
+        //public IRepository<DataAccess.User_Survey> UserSurveyRep { get { return _user_survey; } }
+
+
+        public SurveyEfDbContextLocator()
+        {
+            _dbContext = new SurveyEntities();
+            _surveyRep = new BaseEFRepository<DataAccess.Survey>(DbContext);
+            _user_survey = new BaseEFRepository<DataAccess.User_Survey>(DbContext);
+        }
+             
+
+        public IRepository<DataAccess.Survey> SurveyRep()
+        {
+            return _surveyRep;
+        }
+        public IRepository<User_Survey> UserSurveyRep => throw new NotImplementedException();
+    }
+}
