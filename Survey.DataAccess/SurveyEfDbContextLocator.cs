98bc1e791c4aaecf37505e33f1aa79d456f2d231
shahrzadabedi <shahrzad.abedi@gmail.com>
2020-11-14 18:45:50 +0330

    [revision]DB AND Data structure change

--- Survey.DataAccess/SurveyEfDbContextLocator.cs
+++ Survey.DataAccess/SurveyEfDbContextLocator.cs
@@ -10,14 +10,14 @@ using System.Threading.Tasks;
 
 namespace Survey.DataAccess
 {
-    public class SurveyEfDbContextLocator : IEFRepositoryLocator<Ef.DB.Survey, Ef.DB.UserAnswer,Ef.DB.Question>
+    public class SurveyEfDbContextLocator : IEFRepositoryLocator<Ef.DB.Survey, Ef.DB.UserAnswer,Ef.DB.Question,Ef.DB.Answer>
     {
 
         private DbContext _dbContext;
         private IRepository<Ef.DB.Survey> _surveyRep;
-        private IRepository<Ef.DB.UserAnswer> _surveyQAnswer;
+        private IRepository<Ef.DB.UserAnswer> _userAnswer;
         private IRepository<Ef.DB.Question> _question;
-       
+        private IRepository<Ef.DB.Answer> _answer;
 
 
 
@@ -26,7 +26,7 @@ namespace Survey.DataAccess
         {
             _dbContext = new SurveyContext();
             this._surveyRep = new BaseEFRepository<Ef.DB.Survey>(_dbContext);
-            this._surveyQAnswer = new BaseEFRepository<Ef.DB.UserAnswer>(_dbContext);
+            this._userAnswer = new BaseEFRepository<Ef.DB.UserAnswer>(_dbContext);
 
 
         }
@@ -42,7 +42,9 @@ namespace Survey.DataAccess
             _dbContext.SaveChanges();
         }
 
-        public IRepository<Ef.DB.UserAnswer> SurveyQAnswerRep() { return _surveyQAnswer; }
+        public IRepository<Ef.DB.UserAnswer> UserAnswerRep() { return _userAnswer; }
         public IRepository<Ef.DB.Question> QuestionRep() { return _question; }
+
+        public IRepository<Answer> AnswersRep()  { return _answer;   }
     }
 }
